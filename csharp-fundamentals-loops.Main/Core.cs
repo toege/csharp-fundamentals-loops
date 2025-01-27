f6d74d9969d7af1968274e5548b3c66e2a431046
Tonnes <to.smith.eg@gmail.com>
2025-01-27 12:06:50 +0100

    Done

--- csharp-fundamentals-loops.Main/Core.cs
+++ csharp-fundamentals-loops.Main/Core.cs
@@ -19,33 +19,56 @@ namespace csharp_fundamentals_loops.Main
 
         public void stepOne()
         {
-            // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array                        
-            throw new NotImplementedException();            
+            // TODO: 1. Write a for loop that adds the numbers 0 to 3 to the numsZeroToThree array 
+            for (int i = 0; i < 4; i++)
+            {
+                numsZeroToThree[i] = i;
+            }
         }
 
         public void stepTwo()
         {
             // TODO: 2. Write a for loop that adds the numbers 5 to 10 to the numsFiveToTen array
-            throw new NotImplementedException();
+            for (int i = 0; i < 6; i++)
+            {
+                numsFiveToTen[i] = i+5;
+            }
         }
 
         public void stepThree()
         {
             // TODO: 3. Write a for loop that adds the numbers 3 to 0 (in that order) to the countdown array
-            throw new NotImplementedException();
+            for (int i = 0; i < 4; i++)
+            {
+                countdown[i] = 3-i;
+            }
 
         }
 
         public bool stepFour(int num)
         {
             // TODO: 6. Write a for loop that checks if num is in the favouriteNumbers array            
-            throw new NotImplementedException();
+            foreach (int element in favouriteNumbers)
+            {
+                if (num == element)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
 
         public bool stepFive(string hobby)
         {
             // TODO 5. Write a for loop that checks if the hobby String is in the myHobbies array
-            throw new NotImplementedException();
+            foreach (string element in myHobbies)
+            {
+                if (hobby == element)
+                {
+                    return true;
+                }
+            }
+            return false;
         }
     }
 
